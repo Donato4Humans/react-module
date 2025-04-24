688301bcfee9c4c1f3e863a6848a837fb53e0b43
Donato4Humans <donato.4work@gmail.com>
2025-04-24 21:21:56 +0300

    - added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-
+        <MyComponent text = {'hello react1'}/>
+        <MyComponent text = {'hello react2'}/>
+        <MyComponent text = {'hello react3'}/>
     </>
   )
 }
