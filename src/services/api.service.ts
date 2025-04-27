fcb8312ec5d929465d75f6af8179cd4cfa22487a
Donato4Humans <donato.4work@gmail.com>
2025-04-28 01:57:22 +0300

    - finished env-files topic in lesson2

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,12 +3,12 @@ import {IUser} from "../models/IUser.ts";
 // WE ENCAPSULATE IN THIS SERVICE LAYER FILE ALL ASYNC API CALLS AND EXPORT THEM
 
 const getUsers = async (): Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(value => value.json());
+    return await fetch(import.meta.env.VITE_API_URL) // TO PREVENT DUPLICATING URL OR OTHER CONSTANTS
+        .then(value => value.json()); // WE WILL USE .env FILE AND CREATE THERE CONSTANTS AND CALL THEM FROM THERE
 }
 
 const getUser = async (id:string): Promise<IUser> => {
-    return await fetch(`https://jsonplaceholder.typicode.com/users` + id)
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then(value => value.json());
 }
 
