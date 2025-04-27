60fa0429ce9edf093ff2490face9cb83ba353d8f
Donato4Humans <donato.4work@gmail.com>
2025-04-28 02:51:23 +0300

    - finished state-lift topic in lesson2

--- src/components/user-component/UserComponent.tsx
+++ src/components/user-component/UserComponent.tsx
@@ -2,15 +2,21 @@ import {FC} from "react";
 import {IUser} from "../../models/IUser.ts";
 
 type UserPropsType = {
-    item: IUser
+    item: IUser,
+    details: (item: IUser) => void
 }
 
-const UserComponent: FC<UserPropsType> = ({item}) => {
+const UserComponent: FC<UserPropsType> = ({item, details}) => {
     return (
-        <div>
-            {item.name}
-        </div>
-    );
+        <>
+            <div>
+                {item.name}
+            </div>
+            <button className='border-2' onClick={() => {
+                details(item);
+            }}>details</button>
+        </>
+);
 };
 
 export default UserComponent;
--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -4,31 +4,33 @@ import UserComponent from "../user-component/UserComponent.tsx";
 import {getUsers} from "../../services/api.service.ts";
 
 const UsersComponent = () => {
-    const [users, setUsers] = useState<IUser[]>([]); // we save state of empty IUser[] to fill it after fetch will get data
+    const [users, setUsers] = useState<IUser[]>([]);
+    const [user, setUser] = useState<IUser | null>(null); // WE USE NULL AS INITIAL STATE SO WE CAN CHECK LATER
+                                                                   // AND DECIDE TO RENDER NEW ELEMENT OR NO
 
-    // we use useEffect hook to prevent useState hook from infinite rerender page loop after fetch response will be done
-    useEffect(() => { // BECAUSE COMPONENTS ARE UI-BIASED, WE NEED TO ENCAPSULATE FETCH IN service layer file(api.service)
+    useEffect(() => {
         getUsers()
             .then(response => {
                 setUsers(response);
             });
 
-            // const fetchData = async () => {  OTHER APPROACH WITHOUT .THEN CALL AND WITH ASYNC-AWAIT
-            //     const users = await getUsers();
-            //     setUsers(users);
-            // }
-            // fetchData();
-
-        return () => { // callback func that will be called after useEffect finished work(so called "unsubscribe func"
+        return () => {
             console.log('done fetch');
         }
-    }, []); // here we have empty dependencies array that can be used to repeat call of fetch or other code scoped inside
-            // useEffect hook, if one of variables(or reference to obj) within this array will be changed
+    }, []);
+
+    const userDetails = (item: IUser) => { // func that will lift data from child component to parent
+        setUser(item);
+    }
 
     return (
         <div>
             {
-                users.map((user) => <UserComponent key={user.id} item={user}  />)
+                user && <div>{JSON.stringify(user)}</div> // simplified item details to show concept and check if user exist
+            }
+            {
+                users.map((user) => <UserComponent details={userDetails} key={user.id} item={user}  />)
+                                                                 // here we send func reference into component props
             }
         </div>
     );
