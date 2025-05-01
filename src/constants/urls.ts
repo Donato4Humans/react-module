34936577a680385379e5b72239391a003341b904
Donato4Humans <donato.4work@gmail.com>
2025-05-01 16:11:31 +0300

    - finished task3 from hw3 - changed urls.ts to get all obj from dummyjson api

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -4,14 +4,14 @@ const baseUrlD = 'https://dummyjson.com';
 export const urls = {
     users: {
         allUsersPH: baseUrlPH + '/users',
-        allUsersD: baseUrlD + '/users',
+        allUsersD: baseUrlD + '/users?limit=0',
     },
     posts: {
         allPostsPH: baseUrlPH + '/posts',
-        allPostsD: baseUrlD + '/posts',
+        allPostsD: baseUrlD + '/posts?limit=0',
     },
     comments: {
         allCommentsPH: baseUrlPH + '/comments',
-        allCommentsD: baseUrlD + '/comments',
+        allCommentsD: baseUrlD + '/comments?limit=0',
     }
 }
