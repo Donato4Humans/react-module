776f263d0ad8338c84051b4d4ff80ae662f997bd
Donato4Humans <donato.4work@gmail.com>
2025-05-02 15:41:35 +0300

    - finished useForm-hook topic in lesson5

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,47 +1,25 @@
-import {FormEvent, useState} from "react";
+import {useForm} from "react-hook-form";
 
 interface IFormProps {
     username: string;
     password: string;
+    age: number;
 }
 
 const FormComponent = () => {
 
-    const [formState, setFormState] = useState<IFormProps>({ // set default values of form
-            username: 'foobar',
-            password: '1111'
-    })
+    const {handleSubmit, register} = useForm<IFormProps>();
 
-    const handleSumbit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const user = {
-            username: formState.username,
-            password: formState.password,
-        };
-        console.log(user, 'user sent to API');
-    }
-
-    // ABOVE ARE TWO OLD AND NOT USED NOW FOR FORMS INPUT CONTROL APPROACHES-----------------------------------------
-
-    // const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => { // FIRST APPROACH
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, username: input.value }); // get default state of form and then change field with entered value from form
-    // }
-    // const handlePasswordChange = (e: FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, password: input.value });
-    // }
-
-    const handleInputChange = (e: FormEvent<HTMLInputElement>) => { // second approach
-        const input = e.target as HTMLInputElement;
-        setFormState({...formState, [input.name]: input.value }); // dynamic change of field name depending on which field changed
+    const customHandler = (formDataProps:IFormProps) => {
+        console.log(formDataProps);
     }
 
     return (
         <div>
-            <form onSubmit={handleSumbit}>
-                <input className='border-2' type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
-                <input className='border-2' type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <input className='border-2' type="text" {...register('username')}/>
+                <input className='border-2' type="text" {...register('password')}/>
+                <input className='border-2' type="number" {...register('age')}/>
                 <button className='border-2'>send</button>
             </form>
         </div>
